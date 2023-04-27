f1a084026b89538e43cb3aec90d3e079658dec4d
muk7777 <muk8620@gmail.com>
2023-04-27 20:16:15 +0900

    mysql connect j

--- src/com/KoreaIT/JAM/Main.java
+++ src/com/KoreaIT/JAM/Main.java
@@ -1,12 +1,20 @@
 package com.KoreaIT.JAM;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
+import com.KoreaIT.JAM.dto.Article;
+
 public class Main {
+	
+	static int lastArticleId;
+	
 	public static void main(String[] args) {
 		System.out.println("== 프로그램 시작 ==");
 		
 		Scanner sc = new Scanner(System.in);
+		List<Article> articles = new ArrayList<>();
 		
 		while (true) {
 			System.out.printf("명령어) ");
@@ -15,6 +23,35 @@ public class Main {
 			if (cmd.equals("exit")) {
 				System.out.println("== 프로그램 끝 ==");
 				break;
+			}
+			
+			if (cmd.equals("article write")) {
+				System.out.println("== 게시물 작성 ==");
+				System.out.printf("제목 :");
+				String title = sc.nextLine();
+				System.out.printf("내용 :");
+				String body = sc.nextLine();
+				
+				int id = ++lastArticleId;
+				
+				Article article = new Article(id, title, body);
+				articles.add(article);
+				
+				System.out.printf("%d번 게시물이 작성되었습니다.\n", id);
+				
+			} else if (cmd.equals("article list")) {
+				System.out.println("== 게시물 리스트 ==");
+				
+				if (articles.size() == 0) {
+					System.out.println("존재하는 게시물이 없습니다.");
+					continue;
+				}
+				
+				System.out.println("번호	|	제목");
+				for (Article article : articles) {
+					System.out.printf("%d	|	%s\n", article.id, article.title);
+				}
+				
 			}
 		}
 		
